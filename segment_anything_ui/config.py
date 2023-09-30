bb5ad5e23a52993c8b5d711e9b682f00d6cece54
Branislav Hesko <branislav.hesko@proton.me>
2023-09-30 10:46:48 +0200

    added keymap to config

--- segment_anything_ui/config.py
+++ segment_anything_ui/config.py
@@ -1,12 +1,40 @@
 import dataclasses
 import os
 
+from PySide6.QtCore import Qt
+
+
+@dataclasses.dataclass(frozen=True)
+class Keymap:
+    key: Qt.Key
+    name: str
+
+
+@dataclasses.dataclass
+class KeyBindings:
+    ADD_POINT: Keymap = Keymap(Qt.Key.Key_W, "W")
+    ADD_BOX: Keymap = Keymap(Qt.Key.Key_Q, "Q")
+    ANNOTATE_ALL: Keymap = Keymap(Qt.Key.Key_Return, "Enter")
+    MANUAL_POLYGON: Keymap = Keymap(Qt.Key.Key_R, "R")
+    CANCEL_ANNOTATION: Keymap = Keymap(Qt.Key.Key_C, "C")
+    SAVE_ANNOTATION: Keymap = Keymap(Qt.Key.Key_S, "S")
+    PICK_MASK: Keymap = Keymap(Qt.Key.Key_X, "X")
+    MERGE_MASK: Keymap = Keymap(Qt.Key.Key_Z, "Z")
+    DELETE_MASK: Keymap = Keymap(Qt.Key.Key_V, "V")
+    PARTIAL_ANNOTATION: Keymap = Keymap(Qt.Key.Key_D, "D")
+
+    NEXT_FILE: Keymap = Keymap(Qt.Key.Key_F, "F")
+    PREVIOUS_FILE: Keymap = Keymap(Qt.Key.Key_G, "G")
+    SAVE_MASK: Keymap = Keymap(Qt.Key.Key_S, "Ctrl+S")
+    PRECOMPUTE: Keymap = Keymap(Qt.Key.Key_P, "P")
+
 
 @dataclasses.dataclass
 class Config:
+    default_weights: str = "sam_vit_b_01ec64.pth"
+    label_file: str = "labels.json"
     window_size: tuple[int, int] | int = (1600, 900)
-    default_weights = "sam_vit_b_01ec64.pth"
-    label_file = "labels.json"
+    key_mapping: KeyBindings = dataclasses.field(default_factory=KeyBindings)
 
     def __post_init__(self):
         if isinstance(self.window_size, int):
