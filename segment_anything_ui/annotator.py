4a1c039657285c865dea79d1ce533d0127853380
Branislav Hesko <branislav.hesko@protonmail.ch>
2024-07-18 14:37:10 +0200

    pyplot fix

--- segment_anything_ui/annotator.py
+++ segment_anything_ui/annotator.py
@@ -18,8 +18,10 @@ from segment_anything_ui.utils.shapes import BoundingBox
 def get_cmap(n, name='hsv'):
     '''Returns a function that maps each index in 0, 1, ..., n-1 to a distinct
     RGB color; the keyword argument name must be a standard mpl colormap name.'''
-    return plt.cm.get_cmap(name, n)
-
+    try:
+        return plt.cm.get_cmap(name, n)
+    except:
+        return plt.get_cmap(name, n)
 
 def crop_image(
         image,
